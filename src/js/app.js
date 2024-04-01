77dd9a7caecd58497c254c560f6f5edf50e8758f
AliveIsHere <amirdev2006@gmail.com>
2024-04-01 12:55:56 -0700

    updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
 alert("hello world");
+
+const BTN_LOGIN = document.getElementById("btn-login");
+const BTN_LOGIN_google = document.getElementById("btn-login-google");
+const forgot = document.getElementById("forgot");
+
+
+
+BTN_LOGIN.addEventListener(`click`, btn);
+forgot.addEventListener(`click`, forg);
+
+function btn()
+{
+    alert("this site is empty");
+
+}
+
+function forg()
+{
+    alert("this site is empty");
+}
+
+BTN_LOGIN_google.addEventListener(`click`, buttongoogle);
+
+function buttongoogle()
+{
+    alert("this site is empty")
+}
